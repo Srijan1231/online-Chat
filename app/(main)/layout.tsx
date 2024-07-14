884063ea28a4725d28e38aa127cd550356bd489b
srijan <srijan.dhl21@gmail.com>
2024-07-14 23:46:29 +1000

    visible,md:visible,md:flex degudded

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,12 +6,13 @@ export default function ServerLayout({
   }: {
     children: React.ReactNode;
   }) {
+  
     return (
       <div className="h-full">
-        <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
-          <NavigationSidebar />
+        <div className="invisible md:visible md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar /> 
         </div>
-        <main className="md:pl-[72px] h-full">{children}</main>
+        <main className="md:pl-[72px]  h-full">{children}</main>
       </div>
     );
   }
